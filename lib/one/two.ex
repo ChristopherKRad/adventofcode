bac62aeff480393fefbb8ff4aa2f43226aa1243e
Christopher K Rad <chris@cracustom.com>
2023-12-06 13:08:41 -0800

    Day1 (Pt.2) -> Solved

--- lib/one/two.ex
+++ lib/one/two.ex
@@ -3,28 +3,17 @@ defmodule Adventofcode.One.Two do
   iex> Adventofcode.One.Two.file_to_value("path/to/one.txt")
   """
 
-  def file_to_value(file_path) do
-    file_path
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
-    |> Enum.filter(&(&1 != ""))
-    |> calibrate
-  end
-
-  def calibrate(list) do
-    list
-    |> Enum.map(&parse_string/1)
-  end
-
-  @number_words %{
-    "oneight" => 18,
-    "twone" => 21,
-    "threeight" => 38,
-    "fiveight" => 58,
-    "sevenine" => 79,
-    "eightwo" => 82,
-    "eighthree" => 83,
-    "nineight" => 98,
+  @regex ~r/(?=(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))/i
+  @number_map %{
+    "1" => 1,
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
     "one" => 1,
     "two" => 2,
     "three" => 3,
@@ -33,28 +22,46 @@ defmodule Adventofcode.One.Two do
     "six" => 6,
     "seven" => 7,
     "eight" => 8,
-    "nine" => 9,
-    "zero" => 0
+    "nine" => 9
   }
-  def parse_string(string) do
-    Enum.reduce(@number_words, string, fn {word, number}, acc ->
-      String.replace(acc, word, Integer.to_string(number))
-    end)
-    |> String.replace(~r/[^\d]/, "")
-    |> extract_ints()
+
+  def file_to_value(file_path) do
+    file_path
+    # Take the file
+    |> File.stream!()
+    # Trim each row of the file into a list of strings
+    |> Enum.map(&String.trim/1)
+    |> Enum.filter(&(&1 != ""))
+    # Pass each string through the "calibration" to extract the digits
+    |> Enum.map(&calibrate/1)
+
+    # Add them up!
+    |> Enum.sum()
   end
 
-  defp extract_ints(ints) do
-    digits = String.graphemes(ints)
+  def calibrate(string) do
+    # Scan the full string for all values in the number_map 
+    Regex.scan(@regex, string)
+    |> Enum.flat_map(fn matches ->
+      Enum.filter(matches, fn match -> match != "" end)
+    end)
+    # Convert those values into integers
+    |> Enum.map(fn match ->
+      @number_map[match]
+    end)
+    # 'Concat' the first and last if match > 2 elements, duplicate if match < 2
+    |> case do
+      # This case is prob not necessary
+      [] ->
+        0
 
-    case digits do
-      [_single_digit] ->
-        String.to_integer(Enum.join(digits) <> Enum.join(digits))
+      [single_digit] ->
+        single_digit * 11
 
-      _ ->
-        first_digit = Enum.fetch!(digits, 0)
+      digits ->
+        first_digit = List.first(digits) * 10
         last_digit = List.last(digits)
-        String.to_integer(first_digit <> last_digit)
+        first_digit + last_digit
     end
   end
 end
