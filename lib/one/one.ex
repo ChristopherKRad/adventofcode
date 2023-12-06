c857be74d9dec2eaa844b4d684d06018215d8f2a
Christopher K Rad <chris@cracustom.com>
2023-12-05 17:18:42 -0800

    Day 1 -> Not Solved

--- lib/one/one.ex
+++ lib/one/one.ex
@@ -9,4 +9,28 @@ defmodule One do
     |> Enum.map(&String.trim/1)
     |> Enum.filter(&(&1 != ""))
   end
+
+  def calibrate(list) do
+    list
+    |> Enum.map(&extract_ints/1)
+
+    # |> Enum.sum()
+  end
+
+  defp extract_ints(string) do
+    digits = String.graphemes(string) |> Enum.filter(&String.match?(&1, ~r/\d/))
+
+    case digits do
+      [] ->
+        0
+
+      [_single_digit] ->
+        String.to_integer(Enum.join(digits) <> Enum.join(digits))
+
+      _ ->
+        min_digit = Enum.min(digits)
+        max_digit = Enum.max(digits)
+        String.to_integer(min_digit <> max_digit)
+    end
+  end
 end
