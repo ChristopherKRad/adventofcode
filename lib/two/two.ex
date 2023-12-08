0aae070933412c517969e6fe6f6645f9933a79b4
Christopher K Rad <chris@cracustom.com>
2023-12-07 16:45:16 -0800

    Day2 (Pt.2) -> Solved

--- lib/two/two.ex
+++ lib/two/two.ex
@@ -1,4 +1,4 @@
-defmodule Adventofcode.Two.One do
+defmodule Adventofcode.Two.Two do
   @doc """
   iex> Adventofcode.Two.One.file_to_value("path/to/two.txt")
   """
@@ -9,6 +9,7 @@ defmodule Adventofcode.Two.One do
     |> Enum.map(&String.trim/1)
     |> Enum.filter(&(&1 != ""))
     |> Enum.map(&parse_game/1)
+    |> sum_game_powers()
   end
 
   # Parse the list of strings into a tuple {game_id, game_data}
@@ -19,7 +20,6 @@ defmodule Adventofcode.Two.One do
   end
 
   # Split the game_data into a list of rounds
-  # process the rounds into maps
   def extract_rounds(game_data) do
     game_data
     |> String.split(";")
@@ -27,27 +27,41 @@ defmodule Adventofcode.Two.One do
     |> Enum.map(&process_round/1)
   end
 
+  # Process the rounds into maps
   def process_round(round) do
     round
     |> String.split(",")
     |> Enum.map(&String.trim/1)
-    |> Enum.reduce(%{"red" => 0, "blue" => 0, "green" => 0}, fn color_count_str, acc ->
-      [count_str, color] = String.split(color_count_str, " ")
+    |> Enum.reduce(%{"red" => 0, "blue" => 0, "green" => 0}, fn color_n_count, acc ->
+      [count_str, color] = String.split(color_n_count, " ")
       count = String.to_integer(count_str)
       Map.update(acc, color, count, &(&1 + count))
     end)
   end
 
   # -------------
-  # For each list of rounds ->
-  # Find the "minimum number required per color" (i.e. the max for each color ) "color_cap"
-  # Store each color_cap in a map
-  # Multiply the color_caps for each list of rounds
-  # Sum the power of each list of rounds together
+  # Games are now formatted as follows: {id, [%{round1}, %{round2}, ...]}
   # -------------
 
-  # Sum the powers of each game to get the result
-  # def sum_tuples(tuples) do
-  #   Enum.reduce(tuples, 0, fn {_, second_value}, acc -> acc + second_value end)
-  # end
+  def sum_game_powers(games) do
+    Enum.map(games, fn {_game_id, rounds} ->
+      rounds
+      |> max_color_counts_per_game()
+      |> calculate_color_cap_power()
+    end)
+    # Add up the result!
+    |> Enum.sum()
+  end
+
+  # Iterate through each round, and gather the max color count per game
+  defp max_color_counts_per_game(rounds) do
+    Enum.reduce(rounds, %{"red" => 0, "blue" => 0, "green" => 0}, fn round, acc ->
+      Map.merge(acc, round, fn _key, v1, v2 -> max(v1, v2) end)
+    end)
+  end
+
+  # Gather the 'power' for each game
+  defp calculate_color_cap_power(color_counts) do
+    Enum.reduce(color_counts, 1, fn {_color, count}, acc -> acc * count end)
+  end
 end
